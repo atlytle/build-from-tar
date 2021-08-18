be9798f7741e1e3b0fd0bdf3eee7dcab7d325d25
Andrew Lytle <atlytle@lq.fnal.gov>
2021-08-18 15:13:11 -0500

    Add source

---
+++ extract_milc_corrs.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+# Andrew Lytle
+# March 2015
+
+import os
+import re
+import subprocess
+import sys
+from glob import glob
+from multiprocessing import Pool
+from time import time
+
+import numpy as np
+
+sys.path.append(os.environ['HOME'] + '/allhisq')
+from timing import timing
+
+REGEX = re.compile('correlator_key:')  # Pattern signaling end of header.
+T = 48
+
+def sort_argv(argv):
+    "Sort arguments in *.*.cfg_tsrc_* by cfg then src."
+    def sort_fun(arg):
+        arg = os.path.basename(arg)  # Remove path.
+        sarg = arg.split('_')
+        cfg = int(sarg[0].split('.')[-1])
+        src = int(sarg[1].lstrip('t'))
+        return (cfg, src)
+
+    return sorted(argv, key=sort_fun)
+
+def remove_headers(lines):
+    "Lines corresponding to correlation functions."
+    result = []
+    for i, line in enumerate(lines):
+        if REGEX.match(line):
+            # Skip "..." line after the match.
+            result.append(lines[i+2:i+2+T])
+    return result
+
+def real_part(lines):
+    "Real part of correlation functions."
+    return [float(line.split()[1]) for line in lines]
+
+def extract(fname):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+    _corr_key = corr_key(lines)
+    lines = remove_headers(lines)
+    nums = np.array(list(map(real_part, lines)))
+    return _corr_key, np.average(nums, axis=0)  # I think this averages over time sources..
+
+def corr_key(lines):
+    """Extract the correlator key from milc output file. 
+    
+    e.g. correlator_key:     P5-P5...
+    """
+    for line in lines:
+        if REGEX.match(line):
+            return line.strip().split()[-1]
+
+# v Is there a native version of this? v
+def get_dirs(loc):
+    res = subprocess.run(["ls", loc], capture_output=True)
+    dirs = res.stdout.decode().split()
+    return dirs
+
+def traverse_rec(base, level):
+    # General recurse thing
+    pass
+
+def traverse(base):
+    for d in get_dirs(base):
+        for d2 in get_dirs(base+'/'+d):
+            yield base+'/'+d+'/'+d2
+            #for f in get_dirs(base+'/'+d+'/'+d2):
+            #    yield base+'/'+d+'/'+d2+'/'+f
+    
+def _write_all(base):
+    "Write all (loose) correlators corresponding to base."
+    for dir in get_dirs(base):
+        for dir2 in get_dirs(base+'/'+dir):
+            for f in get_dirs(base+'/'+dir+'/'+dir2):
+                loc = base+'/'+dir+'/'+dir2
+                corr_key, corr = extract(loc+'/'+f)
+                #corr = extract(loc+'/'+f)
+                #loc = './loose2/'+dir+'.'+dir2+'.'+f
+                conf_tag = f.split('_')[-1]  # e.g. a001155
+                loc = './loose2/'+corr_key+'_'+conf_tag
+                np.savetxt(loc, corr)
+
+def write_all(bases, _concurrent=False):
+    if _concurrent:
+        pool = Pool()
+        pool.map(_write_all, bases)
+        pool.close()
+    else:
+        for base in bases:
+            _write_all(base)
+
+def remove_loose(base):
+    for d in traverse(base):
+        #print(d)
+        subprocess.run(["rm"] + glob(d+"/*.loose"))
+
+def main(argv):
+    #base = "Job100031_a001120/data/loose"
+    bases = [d+'/data/loose' for d in get_dirs('.') if "Job" in d]
+
+    write_all(bases, _concurrent=True)
+    #for base in bases:
+    #    remove_loose(base)
+    
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
---
+++ write_hdf5.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Andrew Lytle
+# Dec 2020
+
+import os
+from glob import glob
+import subprocess
+import sys
+sys.path.append(os.environ['HOME'] + '/allhisq')
+
+import h5py
+import numpy as np
+
+from timing import timing
+
+def collect(loc, _glob):
+    "Filenames of form loc/glob."
+    res = subprocess.run(["ls"] + glob(loc+'/'+_glob), capture_output=True)
+    res = res.stdout.decode().split()
+    return res
+
+def get_keys(loc):
+    #c = collect(loc, "pi.*-p000.*")  # Assume this is present.
+    c = collect(loc, "P5-P5*_p000_*")  # Assume this is present. 
+    # ^ Fix bug if not ^
+    #nconfs = len(c)
+    conf_tag = c[0].split('_')[-1]  # Get a configuration tag.
+    get_key = lambda path: '_'.join(os.path.basename(path).split('_')[:-1])
+    keys = [get_key(c) for c in collect(loc, "*_"+conf_tag)]
+    #nkeys = len(keys)
+    return keys
+
+#may want to structure "./loose" better to avoid large 'ls's.
+def write_data(loc, f):
+    print("Building hdf5:")
+    for key in get_keys(loc):
+        corrs = collect(loc, key+"*")
+        dat = np.array([np.loadtxt(corr) for corr in corrs])
+        f.create_dataset(key, data=dat)
+        
+def main():
+    f = h5py.File("mytestfile2.hdf5", "w")
+    #f.create_dataset("data", (100,), dtype='i')
+
+    write_data("./loose2", f)
+    
+    f.close()
+
+def test():
+    with h5py.File('mytestfile2.hdf5', 'r') as f:
+        print(f.keys())
+        k = list(f.keys())[0]
+        print(k)
+        print(f[k][0])
+        #print(f["pi.m0.002426-m0.002426-p000.corr2pt"]) #_a001085"])
+
+if __name__ == "__main__":
+    with timing():
+        #main()
+        test()
