a17bc9241dff41e0546b951edc0b92a89623101f
Andrew Lytle <atlytle@lq.fnal.gov>
2022-08-24 14:04:19 -0500

    timing.py Write_print() context manager

--- timing.py
+++ timing.py
@@ -7,6 +7,7 @@
 # + http://hoardedhomelyhints.dietbuddha.com/
 #   2012/12/52python-encapsulating-exceptions-with.html
 
+import sys
 import time
 from contextlib import contextmanager
 
@@ -16,4 +17,14 @@ def timing():
     yield
     end = time.time()
     print("Elapsed time:", end-start, "seconds.")
+
+@contextmanager
+def Write_print(outname):
+    old_stdout = sys.stdout
+    log = open(outname, 'a')
+    print('Redirecting print statements to', outname)
+    sys.stdout = log
+    yield
+    sys.stdout = old_stdout
+    log.close()
 
