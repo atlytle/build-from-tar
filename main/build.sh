cd91238e88fa09f00cfac918db50d86058bcf829
Andrew Lytle <atlytle@lq.fnal.gov>
2023-12-04 15:28:34 -0600

    Specify nice level

--- main/build.sh
+++ main/build.sh
@@ -3,4 +3,4 @@
 script="/lustre1/heavylight/atlytle/build-from-src/main/build_allhisq.py"
 input="build.yaml"
 
-nohup python -u $script $input 2>&1 & #>> $log 2>&1 &
+nohup nice -n 6 python -u $script $input 2>&1 & #>> $log 2>&1 &
